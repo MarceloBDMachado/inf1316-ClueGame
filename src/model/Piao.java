e1a5c740d3895074cc4bec2a737932c6ec956261
MarceloBDMachado <marcelobdmachado@gmail.com>
2026-05-10 20:31:06 -0300

    added commentaries

--- src/model/Piao.java
+++ src/model/Piao.java
@@ -1,21 +1,21 @@
 package model;
 
 class Piao {
-    private final String nomeSuspeito;
+    private final String nomeSuspeito; // Final pois um pião nunca pode se tornar outro
     private Casa posicaoAtual;
 
     Piao(String nomeSuspeito) {
         this.nomeSuspeito = nomeSuspeito;
     }
 
+    // Getters simples pra descobrir de quem é o pião e onde ele está.
     String getNome() {
         return nomeSuspeito;
     }
-
     Casa getPosicaoAtual() {
         return posicaoAtual;
     }
-
+    // Setter para o posição, atualiza a posição atual para ser a nova posição pós movimento
     void setPosicaoAtual(Casa novaPosicao) {
         this.posicaoAtual = novaPosicao;
     }
