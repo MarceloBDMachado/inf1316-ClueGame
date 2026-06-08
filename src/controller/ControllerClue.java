5ad23703aac9b61bbade59bc8ef6883c98e48a02
Eric <146148665+Snere@users.noreply.github.com>
2026-06-07 22:02:11 -0300

    Update ControllerClue.java

--- src/controller/ControllerClue.java
+++ src/controller/ControllerClue.java
@@ -69,4 +69,8 @@ public class ControllerClue {
     public JogoClueInicio getModel() {
         return jogoFacade;
     }
+
+    public java.util.List<model.Carta> obterCartasDoJogadorAtual() {
+        return jogoFacade.getCartasDoJogadorAtual();
+    }
 }
